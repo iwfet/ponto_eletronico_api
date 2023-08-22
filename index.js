29c4cb33a55f0823ca6394658a46fedfd277214f
iwfet <j13vvc@gmail.com>
2023-08-22 13:04:46 -0300

    pai

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const { Pool } = require('pg');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000; // Porta do servidor
+
+const pool = new Pool({
+    user: 'ponto_eletronico',
+    host: 'localhost',
+    database: 'ponto_eletronico',
+    password: 'ponto_eletronico',
+    port: 5432,
+  });
+
+app.use(express.json());
+app.use(cors());
+
+// Rota para inserir um registro no banco de dados
+app.post('/inserir-registro', async (req, res) => {
+    const { acao } = req.body;
+    const data_hora = new Date(); // Obtém a data e hora atuais
+  
+    try {
+      const result = await pool.query(
+        'INSERT INTO registros (acao, data_hora) VALUES ($1, $2) RETURNING *',
+        [acao, data_hora]
+      );
+  
+      res.json(result.rows[0]);
+    } catch (error) {
+      console.error('Erro ao inserir registro:', error);
+      res.status(500).json({ erro: 'Erro ao inserir registro' });
+    }
+  });
+
+// Rota para buscar registros por data
+app.get('/buscar-registros', async (req, res) => {
+  const { data } = req.query;
+  try {
+    const result = await pool.query(
+      'SELECT acao, data_hora FROM registros WHERE DATE(data_hora) = $1',
+      [data]
+    );
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Erro ao buscar registros:', error);
+    res.status(500).json({ erro: 'Erro ao buscar registros' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor está rodando na porta ${port}`);
+});
+
+
+module.exports = app;
